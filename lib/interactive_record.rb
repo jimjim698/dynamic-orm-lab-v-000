4d267c5270306003556f3b8e726113c9df973348
James Lotruglio <jimlotruglio@yahoo.com>
2018-07-25 04:49:36 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -63,9 +63,9 @@ class InteractiveRecord
 
   def self.find_by(hash)
     sql = <<-SQL
-    SELECT * FROM #{table_name} WHERE #{hash.keys[0].to_s} = "hash.values[0].to_s"
+    SELECT * FROM #{table_name} WHERE #{hash.keys[0].to_s} = "#{hash.values[0].to_s}"
     SQL
-binding.pry
+
     DB[:conn].execute(sql)
   end
 
